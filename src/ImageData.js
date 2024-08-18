3e8546f65c2cb143865d783cc941e063ba63f1bf
NICE-97 <pattanawongsangpan@gmail.com>
2024-08-18 17:03:49 +0700

    first commit

---
+++ src/ImageData.js
@@ -0,0 +1,48 @@
+const ImageData = [
+    {
+        title: "น้องหมา",
+        image: "https://cdn.pixabay.com/photo/2024/03/25/20/30/german-shorthaired-pointer-8655457_1280.jpg"
+    },
+    {
+        title: "น้องแมว",
+        image: "https://cdn.pixabay.com/photo/2024/02/15/15/46/cat-8575641_1280.jpg"
+    },
+    {
+        title:"น้องหมี",
+        image:"https://cdn.pixabay.com/photo/2024/06/22/05/25/bear-8845470_1280.jpg"
+    },
+    {
+        title:"น้องนก",
+        image:"https://cdn.pixabay.com/photo/2024/03/02/13/05/orange-parrots-8608540_1280.jpg"
+    },
+    {
+        title:"น้องแมงกะพรุน",
+        image:"https://cdn.pixabay.com/photo/2020/03/19/12/16/jellyfish-4947355_1280.jpg"
+    },
+    {
+        title:"น้องวัว",
+        image:"https://cdn.pixabay.com/photo/2020/10/22/10/28/cow-5675684_1280.jpg"
+    },
+    {
+        title:"น้องกิ่งก่า",
+        image:"https://cdn.pixabay.com/photo/2024/04/22/17/03/ai-generated-8713076_1280.png"
+    },
+    {
+        title:"น้องลิง",
+        image:"https://cdn.pixabay.com/photo/2024/04/18/19/14/monkey-8704855_1280.jpg"
+    },
+    {
+        title:"น้องกระรอก",
+        image:"https://cdn.pixabay.com/photo/2024/02/29/21/39/squirrel-8605258_1280.jpg"
+    },
+    {
+        title:"น้องฮิปโป",
+        image:"https://cdn.pixabay.com/photo/2014/11/03/11/06/hippo-515027_1280.jpg"
+    },
+    {
+        title:"น้องห่าน",
+        image:"https://cdn.pixabay.com/photo/2023/10/13/08/45/geese-8312440_1280.jpg"
+    },
+]
+
+export default ImageData
